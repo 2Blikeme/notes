4e20664f77f1709ac0768488689ab13652188670
d.marusev <banya12370@gmail.com>
2023-07-12 14:56:02 +0300

    init commit

---
+++ src/test/java/com/twobelikeme/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twobelikeme.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
